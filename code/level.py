cbc0196394cedca7b56c701b4e8f5e22fb89be5c
vondev <fergus@vondev.co.nz>
2026-05-07 22:48:58 +0100

    trees have feelings but can be chopped down

--- code/level.py
+++ code/level.py
@@ -15,6 +15,7 @@ class Level:
 		# sprite groups
 		self.all_sprites = CameraGroup()
 		self.collision_sprites = pygame.sprite.Group()
+		self.tree_sprites = pygame.sprite.Group()
 
 		self.setup()
 		self.overlay = Overlay(self.player)
@@ -42,7 +43,7 @@ class Level:
 
 		# trees
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites], obj.name)
+			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
 
 		# wildflowers
 		for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -55,7 +56,11 @@ class Level:
 		# player
 		for obj in tmx_data.get_layer_by_name('Player'):
 			if obj.name == 'Start':
-				self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+				self.player = Player(
+					pos = (obj.x, obj.y), 
+					group = self.all_sprites, 
+					collision_sprites = self.collision_sprites,
+					tree_sprites = self.tree_sprites)
 		Generic(
 			pos = (0, 0),
 			surf = pygame.image.load('../graphics/world/ground.png').convert_alpha(),
